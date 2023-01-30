5a1947979c618dc0a4dea6d353fc194aef31c8fa
gbertin12 <gaelbertin12@gmail.com>
2023-01-30 21:27:09 +0100

    update Dockerfile

--- Makefile
+++ Makefile
@@ -1,23 +1,26 @@
 
-VOLUME_DB = /home/gbertin/data/mariadb
-VOLUME_WP = /home/gbertin/data/wordpress
-IMAGES = 
-CONTAINER = 
-VOLUMES = 
+# VOLUME_DB = /home/gbertin/data/mariadb
+# VOLUME_WP = /home/gbertin/data/wordpress
+VOLUME_DB = /Users/tsawe07/data/mariadb
+VOLUME_WP = /Users/tsawe07/data/wordpress
+
 
 all: 
 	mkdir -p ${VOLUME_DB}
 	mkdir -p ${VOLUME_WP}
-	docker-compose -f srcs/docker-compose.yml up --build -d
+	docker-compose -f srcs/docker-compose.yml up
 
 down: 
-	@docker-compose -f srcs/docker-compose.yml down
+	docker-compose -f srcs/docker-compose.yml down
+
+re:
+	docker-compose -f srcs/docker-compose.yml up --build
 
 clean: down
-	@docker rmi -f $(shell docker images -qa)
-	@docker rm $(shell docker ps -qa)
+	docker rmi -f $(shell docker images -qa)
+	docker rm $(shell docker ps -qa)
 
 vclean: 
 	$(shell rm -rf /Users/tsawe07/data/)
-	@docker rmi -f $(shell docker images -qa)
-	@docker volume rm $(shell docker volume ls -q)
+	docker rmi -f $(shell docker images -qa)
+	docker volume rm $(shell docker volume ls -q)
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-	if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	if [ ! -f /var/lib/mysql/${WORDPRESS_TITLE} ]; then
 
-		mv /var/www/wp-config.php /var/www/wordpress/
-		wp core download --allow-root --path="/var/www/wordpress";
-		cd /var/www/wordpress;
-		# wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=${MYSQL_HOSTNAME} --dbcharset="utf8";
+		#mv /var/www/html/wp-config.php /var/www/html
+		wp core download --allow-root --path="/var/www/html";
+		cd /var/www/html;
+		wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost=${MYSQL_HOSTNAME} --dbcharset="utf8";
 		wp core install --allow-root --url=${WORDPRESS_URL} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_MAIL};
 		wp user create --allow-root ${WORDPRESS_USER} ${WORDPRESS_USER_MAIL} --role="author" --user_pass=${WORDPRESS_USER_PASSWORD}; 
 	fi
